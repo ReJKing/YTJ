98cf88deda1a21ff16a9c1062b15d2393e5669d6
ReJKing <ReJKing@outlook.com>
2018-04-06 15:25:37 +0800

    Initial Commit

---
+++ YTJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTJ
+//
+//  Created by Jacky xu on 2018/4/6.
+//  Copyright © 2018年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
