b78292f174d320835e6e54cb5614115890186225
ReJKing <ReJKing@outlook.com>
2018-04-12 18:03:24 +0800

    。

---
+++ YTJ/Movies/Controllers/MoivesViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoivesViewController.h
+//  YTJ
+//
+//  Created by Jacky xu on 2018/4/5.
+//  Copyright © 2018年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BannerView.h"
+#import "JKBaseTableViewController.h"
+@interface MoivesViewController : JKBaseTableViewController
+@property (weak, nonatomic) IBOutlet BannerView *bannerView;
+
+
+@end
